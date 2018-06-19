e72c91db3fc845e18c44bbd7a60b437351f9e30a
bright niyonzima <brightniyonzima@gmail.com>
2018-06-19 17:50:51 +0300

    fix to graph label

--- resources/views/hospitals/ccecsta_results_column_graph.blade.php
+++ resources/views/hospitals/ccecsta_results_column_graph.blade.php
@@ -38,7 +38,7 @@
         var tooltip = {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-               '<td style="padding:0"><b>{point.y:.1f} students</b></td></tr>',
+               '<td style="padding:0"><b>{point.y:.1f} points</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
